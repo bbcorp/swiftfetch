4b8b20cacc4827d6a147a48d977a3ee8a6c516b8
Bertrand Caplet <bertrand.caplet@chunkz.net>
2019-09-05 16:15:00 +0200

    getModelInfos: fix when no infos found

--- src/getModelInfos.cpp
+++ src/getModelInfos.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 string getModelInfos(void)
 {
-	stringstream streamOut;
-	streamOut << RED << "Model" << RESET << ": ";
+	string stringOut;
+	//streamOut << RED << "Model" << RESET << ": ";
 	list<string> modelInfosFilePaths = {"/sys/devices/virtual/dmi/id/sys_vendor", "/sys/devices/virtual/dmi/id/product_name", "/sys/devices/virtual/dmi/id/product_version"};
         string line;
 	for (string filePath : modelInfosFilePaths)
@@ -19,12 +19,13 @@ string getModelInfos(void)
         	if (modelInfosFile.is_open())
         	{
                 	getline (modelInfosFile,line);
-			streamOut << line << " ";
-                modelInfosFile.close();
+			stringOut += line + " ";
+                	modelInfosFile.close();
         	}
-        	else
-			streamOut << "Unable to open file ";
 	}
-	streamOut << endl;
-	return streamOut.str();
+	if (stringOut.length() > 0) // We did gather model info
+		stringOut = RED + "Model" + RESET + ": " + stringOut;
+		 
+	//streamOut << endl;
+	return stringOut;
 }
